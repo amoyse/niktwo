7e5ae9ea99ec7b086fdfd392bd609c5d2065f198
amoyse <securemoyse127@protonmail.com>
2024-04-26 17:06:54 +0100

    error handling with reqwest response

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,20 @@ fn main() {
     // This prints out the url entered
     println!("{:?}", args.target);
 
-    let response = make_request(&args.target);
+    let request_result = make_request(&args.target);
+
+    let response = match request_result {
+        Ok(response) => response,
+        Err(error) => panic!("Problem with this request: {:?}", error),
+    };
+
     println!("{:?}", response);
 
     
 }
 
 fn make_request(url: &str) -> Result<String, Error> {
+    // make sure to use an exact url, not relative!
     let response = reqwest::blocking::get(url)?;
     response.text()
 }
