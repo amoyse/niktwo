0f1bc9605d2a552bd16aa124e01105fdf088653f
amoyse <securemoyse127@protonmail.com>
2024-04-26 15:40:24 +0100

    basic clap set up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    target: String,
+
+    #[arg(long, action)]
+    ssl: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args)
 }
