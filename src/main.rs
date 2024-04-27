d25d3cf8b1a3502890b0f6dee066ee01344726a9
amoyse <securemoyse127@protonmail.com>
2024-04-28 00:12:28 +0100

    find forms returns vec with details of all forms also created custom structs for form details and input details

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,19 @@ struct Args {
     ssl: bool,
 }
 
+#[derive(Debug)]
+struct InputDetails {
+    input_type: String,
+    name: String,
+}
+
+#[derive(Debug)]
+struct FormDetails {
+    action: Option<String>,
+    method: Option<String>,
+    inputs: Vec<InputDetails>
+}
+
 
 async fn sqli_scan() {
 
@@ -40,7 +53,7 @@ async fn main() {
 
     println!("{:?}", response);
 
-   find_forms(&response); 
+   println!("{:?}", find_forms(&response));
 
    sqli_scan().await;
 
@@ -54,24 +67,43 @@ async fn make_request(url: &str) -> Result<String, reqwest::Error> {
     Ok(body)
 }
 
-fn find_forms(html_content: &str) {
+fn find_forms(html_content: &str) -> Vec<FormDetails> {
     let document = Html::parse_document(html_content);
     let form_selector = Selector::parse("form").unwrap();
     let input_selector = Selector::parse("input").unwrap();
+
+    let mut all_form_details: Vec<FormDetails> = Vec::new();
+
     // find each form on a page
     for form in document.select(&form_selector) {
-        if let Some(action) = form.value().attrs().find(|(attr, _)| attr == &"action") {
-            println!("Found form action: {}", action.1);
+        let mut form_info = FormDetails {
+            action: None,
+            method: None,
+            inputs: Vec::new(),
+        };
+
+        if let Some(action) = form.value().attr("action") {
+            form_info.action = Some(action.to_string());
+            println!("Found form action: {}", action);
         } 
-        if let Some(method) = form.value().attrs().find(|(attr, _)| attr == &"method") {
-            println!("Found form method: {}", method.1);
-        }
+        if let Some(method) = form.value().attr("method") {
+            form_info.method = Some(method.to_string());
+            println!("Found form method: {}", method);
+        } 
         
         // find all input tags in each form
         for input in form.select(&input_selector) {
-            let name = input.value().attr("name").unwrap_or("no name");
-            let input_type = input.value().attr("type").unwrap_or("no type");
+            let name = input.value().attr("name").unwrap_or("no name").to_string();
+            let input_type = input.value().attr("type").unwrap_or("no type").to_string();
             println!("Found input: type={} name={}", input_type, name);
+
+            // Add the found input details to the form_info.inputs vector
+            form_info.inputs.push(InputDetails {
+                input_type,
+                name,
+            });
         }
+        all_form_details.push(form_info);
     }
+    all_form_details
 }
