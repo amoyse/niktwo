7cd434525267b438e48cbfdf0f38e1fad00aac25
amoyse <securemoyse127@protonmail.com>
2024-04-27 22:15:36 +0100

    starting to use tokio instead:

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::future::IntoFuture;
+
 use clap::Parser;
 use reqwest::Error;
 use scraper::{Html, Selector};
@@ -16,13 +18,20 @@ struct Args {
     ssl: bool,
 }
 
-fn main() {
+
+async fn sqli_scan() {
+
+}
+
+
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
     // This prints out the url entered
     println!("URL to target: {:?}", args.target);
 
-    let request_result = make_request(&args.target);
+    let request_result = make_request(&args.target).await;
 
     let response = match request_result {
         Ok(response) => response,
@@ -31,16 +40,21 @@ fn main() {
 
     println!("{:?}", response);
 
-   find_forms(&response); 
+   find_forms(&response).await; 
+
+   sqli_scan().await;
+
+
 }
 
-fn make_request(url: &str) -> Result<String, Error> {
+async fn make_request(url: &str) -> Result<String, reqwest::Error> {
     // make sure to use an exact url, not relative!
-    let response = reqwest::blocking::get(url)?;
-    response.text()
+    let response = reqwest::get(url).await?;
+    let body = response.text().await?;
+    Ok(body)
 }
 
-fn find_forms(html_content: &str) {
+async fn find_forms(html_content: &str) {
     let document = Html::parse_document(html_content);
     let selector = Selector::parse("form").unwrap();
     for element in document.select(&selector) {
